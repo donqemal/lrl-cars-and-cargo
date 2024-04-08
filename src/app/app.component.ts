003b49dbd12603dd37d1179c4115037f6d4f343b
Edon <edon_malushaj@hotmail.com>
2024-04-08 18:32:32 +0200

    Create website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   title = 'LRL-CarsAndCargo';
+  services: any = [
+    {id: 1, description: 'Verkauf'},
+    {id: 2, description: 'Ankauf'},
+    {id: 3, description: 'Transport'},
+    {id: 4, description: 'Support'},
+  ];
+
+  cars: string[] = ['carousel-car1', 'carousel-car2', 'carousel-car3'];
+  brands: string[] = ['audi', 'alfa-romeo', 'bmw', 'mercedes', 'vw', 'renault'];
 }
