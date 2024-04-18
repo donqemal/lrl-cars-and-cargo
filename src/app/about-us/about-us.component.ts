1363a83077bd2ce5476f7d5742379c2a543c2430
Edon <edon_malushaj@hotmail.com>
2024-04-18 20:44:12 +0200

    Add about us page

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
 export class AboutUsComponent {
 
+  protected readonly window = window;
 }
