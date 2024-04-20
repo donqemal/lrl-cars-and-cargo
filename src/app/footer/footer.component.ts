5646ea7aff8e55dde1a28f2b0214cf516af27e2d
Edon <edon_malushaj@hotmail.com>
2024-04-20 14:50:13 +0200

    improve footer for mobile devices

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
-    MatIcon
+    MatIcon,
+    NgIf
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
+  protected readonly window = window;
 }
