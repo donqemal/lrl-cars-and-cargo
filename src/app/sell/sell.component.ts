ef20fb7bd94d88b76a0adb35a66b49a7b152bbd3
Edon <edon_malushaj@hotmail.com>
2024-04-20 19:29:21 +0200

    dont allow to send a mail, if required fields are empty

--- src/app/sell/sell.component.ts
+++ src/app/sell/sell.component.ts
@@ -1,21 +1,25 @@
 import {Component} from '@angular/core';
-import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {KeyValuePipe, NgForOf, NgIf} from "@angular/common";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {KeyValuePipe, NgClass, NgForOf, NgIf} from "@angular/common";
 import {HttpClient, HttpClientModule, HttpHeaders} from "@angular/common/http";
 import {ToastrService} from "ngx-toastr";
 import {MatIcon} from "@angular/material/icon";
 import {FooterComponent} from "../footer/footer.component";
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {MatInput, MatInputModule} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
 
 export type Car = {
   marke: string;
   modell: string;
-  erstzulassung: string;
-  kilometerstand: number;
+  jahrgang: number | null;
+  kilometerstand: number | null;
   zustand: string;
   kraftstoffart: string;
   getriebe: string;
   email: string;
-  bemerkungen: string;
+  bemerkungen?: string;
+  preisvorstellung: number | null;
 }
 
 @Component({
@@ -25,45 +29,53 @@ export type Car = {
     KeyValuePipe,
     NgForOf,
     NgIf,
-    FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
     MatIcon,
-    FooterComponent
+    FooterComponent,
+    MatFormField,
+    MatInput,
+    MatButton,
+    MatFormFieldModule,
+    MatInputModule,
+    NgClass,
+    FormsModule
   ],
   templateUrl: './sell.component.html',
-  styleUrl: './sell.component.scss'
+  styleUrl: './sell.component.scss',
 })
 export class SellComponent {
-  constructor(private http: HttpClient, private fb: FormBuilder, private toast: ToastrService) {
-  }
+  car: Car = {
+    marke: '',
+    modell: '',
+    jahrgang: null,
+    kilometerstand: null,
+    zustand: 'Gut',
+    kraftstoffart: 'Benzin',
+    getriebe: 'Automatik',
+    email: '',
+    preisvorstellung: null,
+    bemerkungen: ''
+  };
 
-  carForm = this.fb.group({
-    marke: ['', Validators.required],
-    modell: ['', Validators.required],
-    erstzulassung: ['', Validators.required],
-    kilometerstand: [null, Validators.required],
-    zustand: ['', Validators.required],
-    kraftstoffart: ['', Validators.required],
-    getriebe: ['', Validators.required],
-    email: ['', Validators.required, Validators.email],
-    bemerkungen: [''],
+  carForm = new FormGroup({
+    marke: new FormControl('', Validators.required),
+    modell: new FormControl('', Validators.required),
+    jahrgang: new FormControl(null, Validators.required),
+    kilometerstand: new FormControl(null, Validators.required),
+    zustand: new FormControl('', Validators.required),
+    kraftstoffart: new FormControl('', Validators.required),
+    getriebe: new FormControl('', Validators.required),
+    preisvorstellung: new FormControl(null, Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    bemerkungen: new FormControl('')
   });
 
+  constructor(private http: HttpClient, private toast: ToastrService) {
+  }
 
-  sendMail(
-    marke: string,
-    modell: string,
-    jahrgang: string,
-    kilometerstand: string,
-    zustand: string,
-    kraftstoffart: string,
-    getriebe: string,
-    email: string,
-    preisvorstellung: string,
-    bemerkungen: string,
-  ) {
-    let url = "https://formspree.io/f/mleqagyd";
+  sendMail() {
+    let url = "https://formspree.io/f/moqgyjrw";
 
     const httpOptions = {
       headers: new HttpHeaders({
@@ -73,25 +85,45 @@ export class SellComponent {
     };
 
     let data = `
-      Marke=${marke}&
-      Modell=${modell}&
-      Jahrgang=${jahrgang}&
-      Kilometerstand=${kilometerstand}&
-      Zustand=${zustand}&
-      Kraftstoffart=${kraftstoffart}&
-      Getriebe=${getriebe}&
-      Email=${email}&
-      Preisvorstellung=${preisvorstellung}&
-      Bemerkungen=${bemerkungen}`;
+      Marke=${this.car.marke}&
+      Modell=${this.car.modell}&
+      Jahrgang=${this.car.jahrgang}&
+      Kilometerstand=${this.car.kilometerstand}&
+      Zustand=${this.car.zustand}&
+      Kraftstoffart=${this.car.kraftstoffart}&
+      Getriebe=${this.car.getriebe}&
+      Email=${this.car.email}&
+      Preisvorstellung=${this.car.preisvorstellung}&
+      Bemerkungen=${this.car.bemerkungen}`;
 
     this.http.post<any>(url, data, httpOptions).subscribe({
       next: () => {
         this.toast.success('Ihre Anfrage wurde erfolgreich versendet!', undefined, {positionClass: 'toast-top-center'});
-        this.carForm.reset();
+        this.car = {
+          marke: '',
+          modell: '',
+          jahrgang: null,
+          kilometerstand: null,
+          zustand: 'Gut',
+          kraftstoffart: 'Benzin',
+          getriebe: 'Automatik',
+          email: '',
+          preisvorstellung: null,
+          bemerkungen: ''
+        }
       },
       error: () => {
         this.toast.error('Ihre Anfrage konnte nicht versendet werden! Bitte versuchen Sie es erneut', undefined, {positionClass: 'toast-top-center'});
       }
     })
+  }
+
+  checkFieldsFilled() {
+    const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.car.email);
+    const areOtherFieldsFilled = Object.keys(this.car)
+      .filter(key => key !== 'bemerkungen')
+      .every(key => (this.car as any)[key]);
+
+    return isEmailValid && areOtherFieldsFilled;
   }
 }
