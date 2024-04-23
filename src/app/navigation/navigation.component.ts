98ad42a8460bbc12185a8b84853f4e999ae5d92b
Edon <edon_malushaj@hotmail.com>
2024-04-23 20:56:30 +0200

    clicking outside of burger menu closes it

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, HostListener} from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
 import {NgIf} from "@angular/common";
 import {Router, RouterLink} from "@angular/router";
@@ -17,7 +17,7 @@ import {Router, RouterLink} from "@angular/router";
 export class NavigationComponent {
   mobileNavigationVisible: boolean = false;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private elementRef: ElementRef) {
   }
 
   toggleMobileNavigation() {
@@ -27,5 +27,15 @@ export class NavigationComponent {
   async navigatePage(page: string) {
     this.mobileNavigationVisible = false;
     await this.router.navigate([page]);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event) {
+    const clickedElement = event.target as HTMLElement;
+    const isInsideIcon = clickedElement.classList.contains('mat-icon');
+
+    if (!this.elementRef.nativeElement.contains(clickedElement) && !isInsideIcon && this.mobileNavigationVisible) {
+      this.toggleMobileNavigation();
+    }
   }
 }
