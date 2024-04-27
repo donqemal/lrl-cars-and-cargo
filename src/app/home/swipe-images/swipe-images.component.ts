04c54bb37b9cddf5ccf928e5593a5819deb70254
Edon <edon_malushaj@hotmail.com>
2024-04-27 22:30:46 +0200

    Add new images/video

--- src/app/home/swipe-images/swipe-images.component.ts
+++ src/app/home/swipe-images/swipe-images.component.ts
@@ -1,13 +1,16 @@
 import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-swipe-images',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './swipe-images.component.html',
   styleUrl: './swipe-images.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SwipeImagesComponent {
-  cars: string[] = ['carousel-car1', 'carousel-car2', 'carousel-car3', 'carousel-car4', 'carousel-car5'];
+  cars: string[] = ['carousel-car1', 'carousel-car2', 'carousel-car3', 'carousel-car4'];
 }
