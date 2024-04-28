af36d0d0f50434cdc5cc49f0bd10063cb6ed5df2
Edon <edon_malushaj@hotmail.com>
2024-04-28 14:26:30 +0200

    Remove unused import

--- src/app/transport/accept/accept.component.ts
+++ src/app/transport/accept/accept.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
+import {Router} from "@angular/router";
 import {Details, User} from "../../model";
 import {MatIcon} from "@angular/material/icon";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
