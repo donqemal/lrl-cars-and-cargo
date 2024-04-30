11a694668d6060a2eab3cf833b686fad2717089c
Edon Malushaj <edon_malushaj@hotmail.com>
2024-04-30 21:41:34 +0200

    Change text

--- src/app/home/services/services.component.ts
+++ src/app/home/services/services.component.ts
@@ -9,7 +9,7 @@ import {Component} from '@angular/core';
 })
 export class ServicesComponent {
   services: any = [
-    {id: 1, description: 'Verkauf', text: 'Verkaufen Sie Ihr Auto einfach und schnell!'},
+    {id: 1, description: 'Verkauf', text: 'Finden Sie jetzt Ihr Traumauto!'},
     {id: 2, description: 'Ankauf', text: 'Wir kaufen Ihr Auto zum Bestpreis!'},
     {id: 3, description: 'Transport', text: 'Transport Ihres Autos zu Ihnen nach Hause!'},
     {id: 4, description: 'Support', text: 'Unterstützung bei allen Fragen rund um Ihr Auto!'},
