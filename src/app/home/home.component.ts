e30ead15b996c014bcb1d5512f2e76779de93f61
Edon Malushaj <edon_malushaj@hotmail.com>
2024-06-06 22:59:07 +0200

    Use animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {FooterComponent} from "../footer/footer.component";
 import {SwipeImagesComponent} from "./swipe-images/swipe-images.component";
 import {BenefitsComponent} from "./benefits/benefits.component";
@@ -22,30 +22,5 @@ import {NgClass} from "@angular/common";
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
-  componentPositions: { [key: string]: number } = {};
-
-  ngOnInit() {
-    this.calculateComponentPositions();
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  onScroll() {
-    this.calculateComponentPositions();
-  }
-
-  calculateComponentPositions() {
-    const components = ['services', 'car-banner', 'benefits', 'swipe-images'];
-    components.forEach(component => {
-      const element = document.querySelector(`app-${component}`);
-      if (element) {
-        const rect = element.getBoundingClientRect();
-        this.componentPositions[component] = rect.top + window.scrollY;
-      }
-    });
-  }
-
-  isComponentVisible(component: string): boolean {
-    return !!this.componentPositions[component] && this.componentPositions[component] < window.innerHeight + window.scrollY;
-  }
+export class HomeComponent {
 }
