2b935faa49dea81ec0a75c6827dd8cb669367995
Edon Malushaj <edon_malushaj@hotmail.com>
2024-10-26 14:50:53 +0200

    Only run animations once

--- src/app/intersection-observer.directive.ts
+++ src/app/intersection-observer.directive.ts
@@ -1,25 +1,47 @@
-import {AfterViewInit, Directive, ElementRef} from '@angular/core';
+import { AfterViewInit, Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appIntersectionObserver]',
   standalone: true
 })
 export class IntersectionObserverDirective implements AfterViewInit {
-  constructor(private el: ElementRef) {
-  }
+  constructor(private el: ElementRef) {}
 
   ngAfterViewInit() {
     const observer = new IntersectionObserver((entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          this.el.nativeElement.classList.add('show');
+          const directionClass = this.getDirectionClass();
+
+          switch (directionClass) {
+            case 'top':
+              this.el.nativeElement.classList.add('from-top');
+              break;
+            case 'left':
+              this.el.nativeElement.classList.add('from-left');
+              break;
+            case 'right':
+              this.el.nativeElement.classList.add('from-right');
+              break;
+            case 'bottom':
+              this.el.nativeElement.classList.add('from-bottom');
+              break;
+            default:
+              this.el.nativeElement.classList.add('from-left'); // Default animation
+          }
           observer.unobserve(this.el.nativeElement);
-        } else {
-          this.el.nativeElement.classList.remove('show');
         }
       });
     });
 
     observer.observe(this.el.nativeElement);
+  }
+
+  private getDirectionClass(): string {
+    if (this.el.nativeElement.classList.contains('animate-top')) return 'top';
+    if (this.el.nativeElement.classList.contains('animate-left')) return 'left';
+    if (this.el.nativeElement.classList.contains('animate-right')) return 'right';
+    if (this.el.nativeElement.classList.contains('animate-bottom')) return 'bottom';
+    return ''; // Default or empty if no directional class is found
   }
 }
