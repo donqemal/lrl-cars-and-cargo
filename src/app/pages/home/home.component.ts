9a8eddbfc5152cf47be892032f5c9eb1c472fa0d
Edon Malushaj <edon_malushaj@hotmail.com>
2025-11-08 11:59:43 +0100

    Add free google reviews

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, Renderer2} from '@angular/core';
 import {FooterComponent} from "../../components/footer/footer.component";
 import {SwipeImagesComponent} from "./swipe-images/swipe-images.component";
 import {BenefitsComponent} from "./benefits/benefits.component";
@@ -24,5 +24,19 @@ import {IntersectionObserverDirective} from "../../directives/intersection-obser
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.loadScript('https://app.reviewconnect.me/embed/5WQYwcO7MTFrnW0b0ypTQa4PLSL02AUZ/widget.js');
+  }
+
+  loadScript(src: string): void {
+    const script = this.renderer.createElement('script');
+    script.src = src;
+    script.type = 'text/javascript';
+    script.async = true;
+    script.defer = true;
+    this.renderer.appendChild(document.body, script);
+  }
 }
